3033fb2887994ca941912016436edaa4ea3a5039
Andreea <andelenasamur@gmail.com>
2023-08-16 13:56:26 +0200

    authenticate and show active user

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { User } from 'src/app/auth/interfaces/user.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-layout-page',
@@ -24,6 +28,20 @@ export class LayoutPageComponent {
       icon: 'search',
       url: './search'
     },
-  ]
+  ];
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  get user():User | undefined {
+    return this.authService.currentUser;
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/login'])
+  }
 
 }
